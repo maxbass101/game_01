4d674efd1f0f4c0629eec7238f44593c1504a44b
maxbass <maxbass101@gmail.com>
2022-08-13 21:34:46 +0300

    finish

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// Получить случайное число от 0 до size-1
+var getRandomNumber = function (size) {
+    return Math.floor(Math.random() * size);
+};
+
+// Вычислить расстояние от клика (event) до клада (target)
+var getDistance = function (event, target) {
+    var diffX = event.offsetX - target.x;
+    var diffY = event.offsetY - target.y;
+    return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
+
+// Получить для расстояния строку подсказки
+var getDistanceHint = function (distance) {
+    if (distance < 10) {
+        return "Обожжёшся!";
+    } else if (distance < 20) {
+        return "Очень горячо";
+    } else if (distance < 40) {
+        return "Горячо";
+    } else if (distance < 80) {
+        return "Тепло";
+    } else if (distance < 160) {
+        return "Холодно";
+    } else if (distance < 320) {
+        return "Очень холодно";
+    } else if (distance < 620) {
+        return "Очень-очень холодно";
+    } else {
+        return "Замёрзнешь !";
+    }
+};
+
+// Создаём переменные
+var width = 800;
+var height = 800;
+var clicks = 10;
+
+// Случайная позиция клада
+var target = {
+    x: getRandomNumber(width),
+    y: getRandomNumber(height)
+};
+
+// Добавляем элементу img обработчик клика
+$('#map').click(function (event) {
+    clicks--;
+    if (clicks > 0) {
+        // Получаем расстояние от места клика до клада
+        var distance = getDistance(event, target);
+
+        // Преобразуем расстояние в подсказку
+        var distanceHint = getDistanceHint(distance);
+
+        // Записываем в элемент #distance новую подсказку
+        $('#distance').text(distanceHint + " " + clicks);
+
+        // Если клик был достаточно близко, поздравляем с победой
+        if (distance < 8) {
+            alert("Клад найден! Сделано кликов: " + clicks);
+        }
+    } else {
+        alert("КОНЕЦ ИГРЫ!!!")
+    }
+
+});
